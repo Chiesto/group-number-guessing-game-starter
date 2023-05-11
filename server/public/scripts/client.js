cf85d695f18e814aa00b2a05076a0989333e761f
Chris Clark <xchrisck@gmail.com>
2023-05-11 15:52:31 -0500

    set up the server, ser up our client, almost got it to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,70 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#form').on('submit', addInput)
+  // getInput()
 }
+let count = 0
+function addInput(event){
+  event.preventDefault()
+
+ let playOne = $('#player-one').val()
+ let playTwo = $('#player-two').val()
+
+ $('#player-one').val('')
+ $('#player-two').val('')
+
+$.ajax({
+  method: 'POST',
+  url: '/history',
+  data:{
+    p1:playOne,
+    p2:playTwo
+  }
+}).then(function(response){
+  console.log('POST pushInput function works!');
+  getHistory();
+}).catch(function(error){
+alert('Error in our pushInput function',error)
+})
+count++
+ 
+
+
+}
+function getHistory(){
+  $.ajax({
+    method:'GET',
+    url: '/history'
+  }).then(function(response){
+    $('#display').append(response);
+  }).catch(function(error){
+    alert('problems in our getInput function - error =>', error);
+  })
+}
+
+
+// function getInput(){
+//   $.ajax({
+//     method:'GET',
+//     url: '/history'
+//   }).then(function(response){
+//     $('#display').append(response);
+//   }).catch(function(error){
+//     alert('problems in our getInput function - error =>', error);
+//   })
+// }
+// let counter = 0;
+// function getHistory(){
+//   counter++;
+  
+//   $.ajax({
+//     method: 'GET',
+//     url: '/history'
+//   }).then(function(response){
+//     $('#total-guesses').append(`
+//     Here are your guesses ${response}. Total number of guesses ${counter}
+//     `)
+//   })
+// }
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
+const randomNumber = require('/Users/chris/Desktop/workspace/Tier2/week7/group-number-guessing-game-starter/server/public/scripts/randomNumber.js')
+
+let winner; 
+
+let history = [];
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -11,7 +16,33 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+//sending the winner results to the client
+app.get('/game', function(req, res){
+  console.log('/game req was made');
+  res.send(winner);
+})
+
+//receiving the full history of all inputs on the server
+app.post('/history', function(req,res){
+
+  history.push(req.body);
+  console.log(history);
+
+})
+
+//sending the full UPDATED history of all inputs
+app.get('/history',function(req,res){
+  res.send(history);
+ 
+console.log('/history ',)
+})
+
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+
+//  if([history.length-1].p1===randomNumber(1,25)){
+//   console.log('BIG MONEY', randomNumber(1,25));
+// }
