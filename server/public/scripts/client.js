d028f4c799070e5fd482725ab3c2a7093a49a3e0
Chris Clark <xchrisck@gmail.com>
2023-05-11 21:50:17 -0500

    got the server to link with the client properly, display's a history of all guesses and displays text if either player 1 or player 2 is too low or too high and a special text if either one is correct

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,47 +1,89 @@
-$(document).ready(handleReady);
+$(document).ready(onReady);
 
-function handleReady() {
-  console.log("jquery is loaded!")
-  $('#form').on('submit', addInput)
-  // getInput()
-}
-let count = 0
-function addInput(event){
-  event.preventDefault()
-
- let playOne = $('#player-one').val()
- let playTwo = $('#player-two').val()
-
- $('#player-one').val('')
- $('#player-two').val('')
-
-$.ajax({
-  method: 'POST',
-  url: '/history',
-  data:{
-    p1:playOne,
-    p2:playTwo
-  }
-}).then(function(response){
-  console.log('POST pushInput function works!');
+function onReady() {
+  console.log("jquery is loaded!");
+  $('#form').on('submit', postHistory);
   getHistory();
-}).catch(function(error){
-alert('Error in our pushInput function',error)
-})
-count++
- 
-
-
+  getPlayerQuotes();
 }
+//add our input to the history array
+function postHistory(){
+
+ let playerOne = $('#player-one').val();
+ let playerTwo = $('#player-two').val();
+
+ $('#player-one').val('');
+ $('#player-two').val('');
+
+  $.ajax({
+    method: 'POST',
+    url: '/history',
+    data:{
+      p1:playerOne,
+      p2:playerTwo
+    }
+  }).then(function(response){
+    console.log('postHistory function works!');
+    getHistory();
+    getPlayerQuotes();
+    }).catch(function(error){
+    alert('Error in our addNumber function',error)
+  })
+}
+
+//getting player history from the server
 function getHistory(){
   $.ajax({
     method:'GET',
     url: '/history'
   }).then(function(response){
-    $('#display').append(response);
+    console.log('getHistory function works!');
+    renderToDom(response);
+    console.log('rendered player history to DOM');
   }).catch(function(error){
-    alert('problems in our getInput function - error =>', error);
+    alert('problems in our getHistory function - error =>', error);
   })
+}
+
+function getPlayerQuotes(){
+  $.ajax({
+    method:'GET',
+    url: '/playerQuotes'
+  }).then(function(response){
+    console.log('getPlayerQuotes function works!');
+    renderPlayerQuotes(response);
+  }).catch(function(error){
+    alert('problems in our getPlayerHistory function - error =>', error);
+  })
+}
+
+function renderPlayerQuotes(quotes){
+  $('#player-quotes').empty();
+  let item = quotes[0];
+  if(item.player1==="PLAYER 1 WINS!"){
+    $('#player-quotes').append(`
+  <div class = "p1-wins"><b> PLAYER ONE WINS!!!!! </b></div> <br> ${item.player2}
+  `)
+  } else if(item.player2==="PLAYER 2 WINS!"){
+    $('#player-quotes').append(`
+  <div class = "p2-wins"><b> PLAYER TWO WINS!!!!! </b></div> <br> ${item.player1}
+  `)
+  } else {
+
+  $('#player-quotes').append(`
+  Player 1: ${item.player1}    Player2: ${item.player2}
+  `)
+  console.log('Rendered player quotes to the DOM');
+  }
+}
+
+function renderToDom(array){
+  $('#total-guesses').empty();
+  for(item of array){
+    $('#total-guesses').append(`
+    <li>Player 1: ${item.p1}    Player2: ${item.p2}</li>
+    `)
+  }
 }
 
 
--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-const randomNumber = require('/Users/chris/Desktop/workspace/Tier2/week7/group-number-guessing-game-starter/server/public/scripts/randomNumber.js')
 
-let winner; 
 
-let history = [];
+const randomNumber = Math.floor(Math.random() * 25) + 1;
+let winner1 = "";
+let winner2 = "";
+const playerQuotes = [{
+    player1:winner1,
+    player2:winner2
+}];
+
+let history = require('./public/historyList');
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
@@ -16,33 +22,66 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
-//sending the winner results to the client
-app.get('/game', function(req, res){
-  console.log('/game req was made');
-  res.send(winner);
-})
-
-//receiving the full history of all inputs on the server
+//receiving the full history of all inputs on the server X
 app.post('/history', function(req,res){
 
   history.push(req.body);
-  console.log(history);
+  console.log('Received player history on server',history);
 
 })
+//send history to another server side file 'history.js' X
+module.exports = history;
 
-//sending the full UPDATED history of all inputs
+//sending the full UPDATED history of all inputs X
 app.get('/history',function(req,res){
   res.send(history);
  
-console.log('/history ',)
+  console.log('Player history being sent from server', history);
 })
 
+// app.post('/playerQuotes', function(req,res){
+
+//   history.push(req.body);
+//   console.log('Received player history on server',history);
+
+// })
+
+function updatePlayerQuotes() {
+  playerQuotes[0].player1 = winner1;
+  playerQuotes[0].player2 = winner2;
+}
+
+//sending the quotes (if player's guess was too high, too low, or right on) to the client
+app.get('/playerQuotes', function(req, res){
+  console.log('/playerQuotes req was made');
+
+
+  let item = history[history.length-1];
+  console.log('here is our item', item);
+    if (Number(item.p1)===randomNumber){
+        winner1 = "PLAYER 1 WINS!";
+    } else if (Number(item.p1)>randomNumber){
+        winner1 = "Player 1 guessed too high";
+    } else if(item.p1<randomNumber){
+        winner1 = "Player 1 guessed too low";
+    } 
+
+    if (Number(item.p2)===randomNumber){
+        winner2 = "PLAYER 2 WINS!";
+    } else if (Number(item.p2)>randomNumber){
+        winner2 = "Player 2 guessed too high";
+    } else if(Number(item.p2)<randomNumber){
+        winner2 = "Player 2 guessed too low";
+    } 
+    updatePlayerQuotes();
+  
+  res.send(playerQuotes);
+
+  console.log('Player quotes', playerQuotes);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
 
 
-//  if([history.length-1].p1===randomNumber(1,25)){
-//   console.log('BIG MONEY', randomNumber(1,25));
-// }
